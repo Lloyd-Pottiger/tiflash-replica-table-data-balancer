78a103bc98234244a1cb4d41aa0ad9017b3a5a5d
CalvinNeo <calvinneo1995@gmail.com>
2024-04-18 16:25:34 +0800

    missing file
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

---
+++ balancer/dead_region.go
@@ -0,0 +1,145 @@
+package balancer
+
+import (
+	client "github.com/Lloyd-Pottiger/tiflash-replica-table-data-balancer/pd_client"
+	"github.com/pingcap/errors"
+	"github.com/pingcap/log"
+	"go.uber.org/zap"
+
+	pdhttp "github.com/tikv/pd/client/http"
+)
+
+func GetRegionsWithTiFlashReplica(pd client.PDClient, tiflashStoreIds map[int64]int) ([]pdhttp.RegionInfo, error) {
+	var result []pdhttp.RegionInfo
+	regions, e := pd.GetRegions()
+	if e != nil {
+		return nil, e
+	}
+	for _, region := range regions {
+		hasReplica := false
+		for _, peer := range region.Peers {
+			_, ok := tiflashStoreIds[peer.StoreID]
+			if ok {
+				hasReplica = true
+				break
+			}
+		}
+		if hasReplica {
+			result = append(result, region)
+		}
+	}
+	return result, nil
+}
+
+func GetRegionsWithTiFlashReplicaLiveness(regions []pdhttp.RegionInfo, tiflashStoreIdMap map[int64]int, offlineStoreIdMap map[int64]int) ([]pdhttp.RegionInfo, []pdhttp.RegionInfo) {
+	var resultAlive []pdhttp.RegionInfo
+	var resultDead []pdhttp.RegionInfo
+	for _, region := range regions {
+		alive := false
+		for _, peer := range region.Peers {
+			_, isTiFlash := tiflashStoreIdMap[peer.StoreID]
+			_, isOffline := offlineStoreIdMap[peer.StoreID]
+			if isTiFlash {
+				if !isOffline {
+					alive = true
+					break
+				}
+			}
+		}
+		if !alive {
+			resultDead = append(resultDead, region)
+		} else {
+			resultAlive = append(resultAlive, region)
+		}
+	}
+	return resultAlive, resultDead
+}
+
+func PickOneTiFlashStore(region *pdhttp.RegionInfo, stores map[int64]pdhttp.StoreInfo, offlineStoreIdMap map[int64]int) *pdhttp.StoreInfo {
+	for storeID, storeInfo := range stores {
+		alreadyHasPeer := false
+		for _, p := range region.Peers {
+			if p.StoreID == storeID {
+				alreadyHasPeer = true
+				break
+			}
+		}
+		_, isOffline := offlineStoreIdMap[storeID]
+		if !isOffline && !alreadyHasPeer {
+			return &storeInfo
+		}
+	}
+	return nil
+}
+
+func ScheduleRegion(pd client.PDClient, offlineStoreIds []int64, offline, dryRun, showOnly bool) error {
+	tiflashStoreIDs, tiflashStoreMap, err := pd.GetAllTiFlashStores("", "")
+	if err != nil {
+		return err
+	}
+	if len(tiflashStoreIDs) < 2 {
+		return errors.New("TiFlash stores less than 2")
+	}
+	log.Info("region schedule run", zap.Any("offlineStoreIds", offlineStoreIds), zap.Bool("dry-run", dryRun), zap.Bool("show-only", showOnly))
+	if dryRun {
+		log.Info("region schedule running in dry-run mode, it will only print the operator commands. If you want to send the operators to PD, add --dry-run=false")
+	}
+	tiflashStoreIdMap := make(map[int64]int)
+	for _, v := range tiflashStoreIDs {
+		tiflashStoreIdMap[v] = 1
+	}
+	offlineStoreIdMap := make(map[int64]int)
+	for _, v := range offlineStoreIds {
+		offlineStoreIdMap[v] = 1
+	}
+
+	tiflashRegions, err := GetRegionsWithTiFlashReplica(pd, tiflashStoreIdMap)
+	if err != nil {
+		return err
+	}
+
+	tiflashDeadRegions, tiflashAliveRegions := GetRegionsWithTiFlashReplicaLiveness(tiflashRegions, tiflashStoreIdMap, offlineStoreIdMap)
+	var tiflashDeadRegionIds []int64
+	var tiflashAliveRegionIds []int64
+
+	for _, region := range tiflashDeadRegions {
+		tiflashDeadRegionIds = append(tiflashDeadRegionIds, region.ID)
+	}
+	for _, region := range tiflashAliveRegions {
+		tiflashAliveRegionIds = append(tiflashAliveRegionIds, region.ID)
+	}
+
+	log.Info("TiFlash region schedule result",
+		zap.Any("tiflashStoreIDs", tiflashStoreIDs),
+		zap.Any("offlineStoreIds", offlineStoreIds),
+		zap.Any("tiflashDeadRegionIds", tiflashDeadRegionIds),
+		zap.Any("tiflashAliveRegions", tiflashAliveRegions),
+		zap.Any("tiflashAliveRegionIds", tiflashAliveRegionIds))
+
+	if offline {
+		for _, v := range offlineStoreIds {
+			log.Info("Delete offline store id",
+				zap.Any("storeID", v))
+			if !dryRun {
+				pd.DeleteStore(v)
+			}
+		}
+	}
+
+	for _, region := range tiflashDeadRegions {
+		pickStore := PickOneTiFlashStore(&region, tiflashStoreMap, offlineStoreIdMap)
+		if pickStore == nil {
+			log.Warn("Failed to find another store to place this region",
+				zap.Any("region", region))
+			continue
+		}
+		log.Info("Found another store to place this region",
+			zap.Any("storeID", pickStore.Store.ID),
+			zap.Any("region", region))
+		if !dryRun {
+			pd.AddCreatePeerOperator(region.ID, pickStore.Store.ID)
+		}
+	}
+
+	return nil
+}
